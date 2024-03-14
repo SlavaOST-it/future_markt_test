a85f1273d0b8d47d33eeeae5c44cf9e8093b6335
SlavaOST <slavaost-it@mail.ru>
2024-03-14 21:23:09 +0300

    feat: add routes

---
+++ src/utils/routes/routes.ts
@@ -0,0 +1,9 @@
+export const PATH = {
+    homepage: '/',
+    about: '/about',
+    mentoring: '/mentoring',
+    events: '/events',
+    cases: '/cases',
+    reviews: '/reviews',
+    contacts: '/contacts',
+}
